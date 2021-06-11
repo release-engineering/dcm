468e9350dbfcd8ef607924bda1cf48c126290c76
Joe Lanford <joe.lanford@gmail.com>
2021-06-11 12:12:52 -0400

    remove migrate --output option (always use YAML to match what add does)

--- cmd/migrate/cmd.go
+++ cmd/migrate/cmd.go
@@ -1,8 +1,6 @@
 package migrate
 
 import (
-	"fmt"
-
 	"github.com/operator-framework/operator-registry/alpha/declcfg"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -13,7 +11,6 @@ import (
 func NewCmd() *cobra.Command {
 	var (
 		migrate action.Migrate
-		format  = "json"
 	)
 	cmd := &cobra.Command{
 		Use:   "migrate <indexImage>",
@@ -21,15 +18,7 @@ func NewCmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			migrate.IndexImage = args[0]
-
-			switch format {
-			case "json":
-				migrate.WriteFunc = declcfg.WriteJSON
-			case "yaml":
-				migrate.WriteFunc = declcfg.WriteYAML
-			default:
-				return fmt.Errorf("invalid output format %q, expected (json|yaml)", format)
-			}
+			migrate.WriteFunc = declcfg.WriteYAML
 
 			if err := migrate.Run(cmd.Context()); err != nil {
 				logrus.New().Fatal(err)
@@ -37,7 +26,6 @@ func NewCmd() *cobra.Command {
 			return nil
 		},
 	}
-	cmd.Flags().StringVarP(&format, "output", "o", format, "Format to use when writing DC files (json|yaml)")
 	cmd.Flags().StringVarP(&migrate.OutputDir, "output-dir", "d", "index", "Directory in which to migrated index as declarative config")
 	return cmd
 }
